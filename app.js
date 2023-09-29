613d1907f9b3975104e4a990070364ad7e273a0b
Shaun Shanil Menezes <shaunshanilmenezes@Shauns-MacBook-Air.local>
2023-09-29 23:00:37 +0800

    basic done text and picture

--- app.js
+++ app.js
@@ -41,27 +41,27 @@ app.post("/get-answer", async (req, res) => {
   }
 });
 
-// //endpoint for image generation
-// app.get("/api/get-painting", async (req, res) => {
-//   try {
-//     if (typeof req.body.prompt === "string") {
-//       const imageCompletion = await openai.Image.create(
-//         (prompt = rew.body.prompt),
-//         (n = 1),
-//         (size = "1024x1024"),
-//         (temperature = 0), // not sure if these two are ok for images and shit
-//         (max_tokens = 100)
-//       );
-//       const generatedImage = response["data"][0]["url"];
-//     }
-//     res.status(200).json({ text: generatedImage });
-//   } catch (error) {
-//     // Handle errors and return an error response if needed
-//     console.error("Error retrieving details:", error);
-//     res.status(403).json({
-//       text: "https://images.dog.ceo/breeds/ridgeback-rhodesian/n02087394_1722.jpg",
-//     });
-//   }
-// });
+//endpoint for image generation
+app.post("/get-art", async (req, res) => {
+  try {
+    if (typeof req.body.prompt === "string") {
+      // const imageCompletion = await openai.Image.create(
+      const imageCompletion = await openai.images.generate({
+        prompt: req.body.prompt,
+        n: 1,
+        size: "1024x1024",
+      });
+
+      const generatedImage = imageCompletion.data;
+      res.status(200).json({ text: generatedImage });
+    }
+  } catch (error) {
+    // Handle errors and return an error response if needed
+    console.error("Error retrieving details:", error);
+    res.status(403).json({
+      text: "https://images.dog.ceo/breeds/ridgeback-rhodesian/n02087394_1722.jpg",
+    });
+  }
+});
 
 export default app;
